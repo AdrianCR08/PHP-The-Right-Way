86308c028e86b7fa2179ac15199ebabd92d03a24
AdrianCR <adrianrmz917@gmail.com>
2024-07-24 17:58:36 -0600

    First commit

---
+++ index.php
@@ -0,0 +1,42 @@
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Title</title>
+</head>
+<body>
+<!--
+$name = 'Gio';
+
+$x = 1;
+$y = &$x;
+
+$x = 3;
+
+$firstName = 'Gio';
+
+//echo 'Hello' . $name . 'User';
+-->
+<h1>
+
+    <?='Hello Wolrd';?>
+
+</h1>
+
+<p>
+    <?php
+    $x = 10;
+    $y = 5;
+
+    echo '<p>' . $x . ', ' . $y . '</p>' // Not recommended
+    ?>
+</p>
+
+
+
+
+</body>
+</html>
+
