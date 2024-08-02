e5863d98392b46ceb6abbd306833f4886a822afa
Adrian Ramos <adrianrmz917@gmail.com>
2024-08-01 22:26:46 -0600

    Imported libraries

--- Portfolio/index.php
+++ Portfolio/index.php
@@ -1,8 +1,13 @@
-<?php
-include 'header.php';
+<?php include 'header.php';?>
 
+<div class="p5 bg-light">
+    <div class="container">
+        <h1 class="display-3">Welcome</h1>
+        <p class="lead">Jumbo Helper Text</p>
+        <hr class="my-2">
+            <p>More Info</p>
+    </div>
+</div>
 
+<?php include 'footer.php' ?>
 
-
-include 'footer.php'
-?>
